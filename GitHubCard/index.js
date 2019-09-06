1b8b2d9b22f5c83ed09df21b183092804e92e1e2
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-09-05 19:01:11 -0700

    i got part one done, my card showed up!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const profileShow = document.querySelector('.cards');
 
 axios
       .get("https://api.github.com/users/abdirahmanfarah")
       .then(response =>{
-          console.log(response.data)
+          console.log(response.data);
 
-          profileShow.appendChild(profileCard(response.data))
-
+          profileShow.appendChild(profileCard(response.data));
+          
           // const newProfileCard = profileCard();
           // profileShow.appendChild(newProfileCard);
 
@@ -17,10 +18,61 @@ axios
           //   const newProfileCard = profileCard(item);
           //   profileShow.appendChild(newProfileCard);
           // });
+          
       })
-      .catch(error => {
-        console.log('No Data', error)
-      });
+      .catch(err => console.log('No Data: ', err)
+      );
+
+      function profileCard(item){
+
+        // Creating Elements 
+        const
+            newCard = document.createElement('div'),
+            newImage = document.createElement('img'),
+            cardInfo = document.createElement('div'),
+            usersName= document.createElement('h3'),
+            usersUserName = document.createElement('p'),
+            location = document.createElement('p'),
+            profile = document.createElement('p'),
+            gitAddress = document.createElement('a'),
+            followers = document.createElement('p'),
+            following = document.createElement('p'),
+            bio = document.createElement('p');
+      
+            // console.log(newCard);
+      
+            // Creating Classes 
+      
+            newCard.classList.add('card');
+            cardInfo.classList.add('card-info');
+            usersName.classList.add('name');
+            usersUserName.classList.add('username');
+      
+            // Append 
+            // profileShow.appendChild(newCard);
+            newCard.appendChild(newImage);
+            newCard.appendChild(cardInfo);
+            cardInfo.appendChild(usersName);
+            cardInfo.appendChild(usersUserName);
+            cardInfo.appendChild(location);
+            cardInfo.appendChild(profile);
+            profile.appendChild(gitAddress);
+            cardInfo.appendChild(followers);
+            cardInfo.appendChild(following);
+            cardInfo.appendChild(bio);
+      
+             //Text Content
+             newImage.setAttribute('src', item.avatar_url );
+            usersName.textContent = `user: ${item.name}`;
+            usersUserName.textContent = `login: ${item.login}`;
+            location.textContent = `location: ${item.location}`;
+            followers.textContent = `followers: ${item.followers}`;
+            following.textContent = `following: ${item.following}`;
+            bio.textContent = `bio: ${item.bio}`;
+      
+            
+            return newCard;
+      }
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -34,7 +86,6 @@ axios
            create a new component and add it to the DOM as a child of .cards
 */
 
-const profileShow = document.querySelector('.cards');
 
 // console.log(profileShow);
 
@@ -50,7 +101,7 @@ const profileShow = document.querySelector('.cards');
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -76,55 +127,9 @@ const followersArray = [];
 
 // console.log(mainH);
 
-function profileCard(item){
 
-  // Creating Elements 
-  const
-      newCard = document.createElement('div'),
-      newImage = document.createElement('img'),
-      cardInfo = document.createElement('div'),
-      usersName= document.createElement('h3'),
-      usersUserName = document.createElement('p'),
-      location = document.createElement('p'),
-      profile = document.createElement('p'),
-      gitAddress = document.createElement('a'),
-      followers = document.createElement('p'),
-      following = document.createElement('p'),
-      bio = document.createElement('p');
 
-      // Creating Classes 
-
-      newCard.classList.add = ('card');
-      cardInfo.classList.add = ('card-info');
-      usersName.classList.add = ('name');
-      usersUserName.classList.add = ('username');
-
-      // Append 
-      // mainH.appendChild(newCard);
-      newCard.appendChild(newImage);
-      newCard.appendChild(cardInfo);
-      cardInfo.appendChild(usersName);
-      cardInfo.appendChild(usersUserName);
-      cardInfo.appendChild(location);
-      cardInfo.appendChild(profile);
-      profile.appendChild(gitAddress);
-      cardInfo.appendChild(followers);
-      cardInfo.appendChild(following);
-      cardInfo.appendChild(bio);
-
-       //Text Content
-      // newImage.src = `${data.avatar_url}`;
-      usersName.textContent = user.name;
-      usersUserName.textContent = user.login;
-      location.textContent = user.locationi;
-      // followers.textContent = {followers};
-      // following.textContent = {following};
-      // bio.textContent = {bio};
-
-      
-      return profileCard;
-}
-
+// profileCard();
 
      
 // profileCard(git);
